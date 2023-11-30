4fc56d70da033845613510d2e97ca1132de4d11d
Genius-hacker <belloabdulsomod05@gmail.com>
2023-11-29 18:04:38 -0800

    pol

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 """print the alphabet in lowercase, not followed by a new line."""
-
+if char != 'e' and char != 'q':
 for letter in range(97, 123):
  print("{}".format(chr(letter)), end="")
